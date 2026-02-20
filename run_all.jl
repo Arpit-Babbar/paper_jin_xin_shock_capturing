c9438559e13af6c5202f8d4ef45bd1bfcd8d2a92
Arpit Babbar <arpitbabbar@gmail.com>
2026-02-20 09:43:02 +0100

    Clean-ups and add run all

---
+++ run_all.jl
@@ -0,0 +1,19 @@
+# Burgers' equation
+include("run/run_burg1d_jin_xin.jl")
+include("run/run_burg1d.jl")
+
+# Buckley-Leverett problem
+include("run/run_bucklev_jin_xin.jl")
+include("run/run_bucklev.jl")
+
+# Woodward-Collela's blast wave problem
+include("run/run_blast_jin_xin.jl")
+include("run/run_blast.jl")
+
+# Sedov's blast wave problem
+include("run/run_sedov_jin_xin.jl")
+include("run/run_sedov.jl")
+
+# Kelvin-Helmholtz instability
+include("run/run_khi_jin_xin.jl")
+include("run/run_khi.jl")
